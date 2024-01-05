5fe1448e005cf2af152a4128cc2c0bf4c4cf6871
tusk <tusk@tusk-gamma.home>
2024-01-05 15:58:46 -0500

    Fixed issue with recursive make calls

--- source/main.c
+++ source/main.c
@@ -3,10 +3,12 @@
 #include "GL/glew.h"
 #include "GLFW/glfw3.h"
 #include "GL/gl.h"
-#include "hamster_crank.h"
+#include "vector_basic.h"
 
 int main()
 {
+    vector_basic vb;
+    vector_basic_init(&vb, 5, sizeof(int), NULL);
     printf("i ran.\n");
     hc_run();
     //glewInit();
